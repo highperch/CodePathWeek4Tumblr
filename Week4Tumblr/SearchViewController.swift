077d5d01ae8afe6d1b0698d4358fe7e43bfcde4e
highperch <jpeng@box.com>
2016-02-24 11:50:38 -0800

    Added delayed load animation to Search
    No fade animation yet for the final screen, but it animates the three
    images during simulated load time.

--- Week4Tumblr/SearchViewController.swift
+++ Week4Tumblr/SearchViewController.swift
@@ -11,20 +11,35 @@ import UIKit
 class SearchViewController: UIViewController {
 
     @IBOutlet weak var searchFeed: UIImageView!
+    @IBOutlet weak var loadingIndicator: UIImageView!
+    
+    //Declaring variables
+    var loading_1: UIImage!
+    var loading_2: UIImage!
+    var loading_3: UIImage!
+    var images: [UIImage]!
+    var animatedImage: UIImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        //Loading image setup
+        loading_1 = UIImage(named: "loading-1")
+        loading_2 = UIImage(named: "loading-2")
+        loading_3 = UIImage(named: "loading-3")
+        images = [loading_1, loading_2, loading_3]
+        animatedImage = UIImage.animatedImageWithImages(images, duration: 1.0)
+        loadingIndicator.image = animatedImage
         // Do any additional setup after loading the view.
     }
     
     override func viewWillAppear(animated: Bool) {
         searchFeed.hidden = true
-        
+        loadingIndicator.hidden = false
     }
     
     override func viewDidAppear(animated: Bool) {
         delay(1.5) { () -> () in
+            self.loadingIndicator.hidden = true
             self.searchFeed.hidden = false
         }
     }
